1e5ed2c20c878501bf6640b973977a99c9b3e897
Lucas Farias <lucasfarias.ln@gmail.com>
2025-12-14 18:48:25 -0300

    fix: update authentication middleware to use default jwt import

---
+++ src/application/middlewares/AuthenticationMiddleware.ts
@@ -0,0 +1,65 @@
+import jwt from 'jsonwebtoken'
+import { env } from '../../config/env'
+import {
+  IData,
+  IMiddleware,
+  IRequest,
+  IResponse,
+} from '../controller/interfaces/IMiddleware'
+
+interface Payload {
+  sub: {
+    id: string
+    email: string
+  }
+  iat: number
+  exp: number
+}
+
+export class AuthenticationMiddleware implements IMiddleware {
+  async handle({ headers }: IRequest): Promise<IResponse | IData> {
+    const { authorization } = headers as Record<string, string>
+
+    if (!authorization) {
+      return {
+        statusCode: 401,
+        body: { error: 'Unauthorized' },
+      }
+    }
+
+    const [bearer, token] = authorization.split(' ')
+
+    try {
+      if (bearer !== 'Bearer') {
+        return {
+          statusCode: 401,
+          body: { error: 'Unauthorized' },
+        }
+      }
+
+      const payload = jwt.verify(token, env.jwtSecret!) as unknown as Payload
+      const { sub } = payload
+
+      if (!sub?.id) {
+        return {
+          statusCode: 401,
+          body: { error: 'Invalid Token' },
+        }
+      }
+
+      return {
+        data: {
+          id: sub.id,
+          email: sub.email,
+        },
+      }
+    } catch (error) {
+      console.error(error)
+
+      return {
+        statusCode: 401,
+        body: { error: 'Unauthorized' },
+      }
+    }
+  }
+}
