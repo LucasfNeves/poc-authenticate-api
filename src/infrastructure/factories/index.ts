9bf2dd1a3d3958e4b0510d52ab9d2e44656f8d7c
Lucas Farias <lucasfarias.ln@gmail.com>
2025-12-14 18:48:38 -0300

    feat: add factories for get user by id controller and authentication middleware

--- src/infrastructure/factories/index.ts
+++ src/infrastructure/factories/index.ts
@@ -1,4 +1,5 @@
 export {
   makeRegisterUserController,
   makeAuthenticateUserController,
+  makeGetUserByIdController,
 } from './user-factory'
--- src/infrastructure/factories/user-factory.ts
+++ src/infrastructure/factories/user-factory.ts
@@ -3,6 +3,8 @@ import { RegisterUseCase } from '../../application/usecase/register'
 import { CreateUserController } from '../../application/controller/register'
 import { AuthenticateUseCase } from '../../application/usecase/authenticate'
 import { AuthenticateUserController } from '../../application/controller/authenticate'
+import { GetUserByIdUseCase } from '../../application/usecase/get-user-by-id'
+import { GetUserByIdController } from '../../application/controller/get-user-by-id-controller'
 import { JwtAdapterImpl } from '../../domain/JwtAdapter'
 
 export const makeRegisterUserController = () => {
@@ -29,4 +31,12 @@ export const makeAuthenticateUserController = () => {
   )
 
   return authenticateUserController
+}
+
+export const makeGetUserByIdController = () => {
+  const usersRepository = new SequelizeUsersRepository()
+  const getUserByIdUseCase = new GetUserByIdUseCase(usersRepository)
+  const getUserByIdController = new GetUserByIdController(getUserByIdUseCase)
+  
+  return getUserByIdController
 }
